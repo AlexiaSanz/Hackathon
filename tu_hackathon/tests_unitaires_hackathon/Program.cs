e6f78f405e6a25e66c805a82767a39a1a56799f4
alexia <alexiasanzdegaldeano@gmail.com>
2022-07-28 18:22:01 +0200

    Installation des packages NuGet dans le bon projet !!

--- tu_hackathon/tests_unitaires_hackathon/Program.cs
+++ tu_hackathon/tests_unitaires_hackathon/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Xunit;
+using FluentAssertions;
+
+public class Initialisation
+{
+    [Fact]
+}
